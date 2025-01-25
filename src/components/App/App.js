1e4f1c135666a956158542880cda7b7b091ff073
kasra torabi <Kasra.torabiii@gmail.com>
2025-01-25 12:19:51 +0330

    //

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Layout from "./../Layout/Layout"
+import { Children } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <div>
+          <h1>layout</h1>
+        </div>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
