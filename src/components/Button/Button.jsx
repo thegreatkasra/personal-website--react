4236b1ebb5583949e53bf22651ef2e79f62e6571
kasra torabi <Kasra.torabiii@gmail.com>
2025-01-25 21:59:11 +0330

    components
    fifty percent of components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+import "./Button.css"
+
+const Button = ({children}) => {
+    return(
+        <button className="button"> {children} </button>
+    )
+}
+
+export default Button;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Header.css"
+import logo from './Svg/logo.svg';
+import Button from "../Button/Button";
 
 const Header = () => {
     return (
-        <div>
-            header
+        <div className="header">
+            <div className="logo">
+                <img src={logo} alt="Logo"/>
+            </div>
+            <div className="navigation">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div>
+                <Button>Download Resume</Button>
+            </div>
         </div>
     )
 }
