75f827e02f7c6dedefe0e961f52981d4da300f0a
kasra torabi <Kasra.torabiii@gmail.com>
2025-01-27 14:53:17 +0330

    footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Footer.css"
+import behance from "./Icons/icons8-behance.svg"
+import facebook from "./Icons/icons8-facebook-logo.svg"
+import instagram from "./Icons/icons8-instagram.svg"
+import {Link} from "react-router-dom"
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer className="footer">
+            <div className="footer-images">
+                <div>
+                    <Link to="https://www.behance.net/kasratorabi1" > <img src={behance}/> </Link>
+                </div>
+                <div>
+                    <Link to="https://www.behance.net/kasratorabi1" > <img src={facebook}/> </Link>
+                </div>
+                <div>
+                    <Link to="https://www.behance.net/kasratorabi1" > <img src={instagram}/> </Link>
+                </div>
+            </div>
+        </footer>
     )
 }
 
