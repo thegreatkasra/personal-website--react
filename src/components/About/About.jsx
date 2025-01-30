0043b1e5c44659f60ed320722d673f98b0ac65a1
kasra torabi <Kasra.torabiii@gmail.com>
2025-01-30 17:01:52 +0330

    animimation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./About.css"
 import Layout from "../Layout/Layout"
+import { gsap } from 'gsap';
+
 
 const About = () =>{
+    
+    const textRef = useRef();
+    const[text, setText] = useState("About Kasra Torabi");
+
+    const handleAnimate = () =>{
+        setText("Dont Touch ! :D");
+        gsap.to(textRef.current, {
+            y: -500,
+            opacity: 0,
+            duration: 4,
+            scale:0.4,
+        })
+    };
+
+    useEffect(() => {
+        gsap.from(textRef.current, {
+            x: -300,
+            opacity: 0,
+            duration: 1,
+            ease: "power2.out",
+        })
+    },[]);
+    
     return (
         <>
         <Layout>
-            About
+            <div className="name" ref={textRef}>
+                <h1 onClick={handleAnimate}>{text}</h1>
+            </div>
         </Layout>
         </>
     )
